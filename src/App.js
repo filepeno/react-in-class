75147300b59cf89fcbd78c615b4f2daa86a86c16
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-15 14:52:39 +0100

    created some components

--- src/App.js
+++ src/App.js
@@ -3,57 +3,77 @@ import "./App.css";
 function App() {
   return (
     <main>
+      <Header />
+      <ProdustList />
+      <Basket />
+    </main>
+  );
+}
+
+function Header() {
+  return (
+    <header>
       <nav>
         <ul>
-          <a href="/">Home</a>
-          <li></li>
-          <li></li>
-          <li></li>
+          <NavLink />
+          <NavLink />
+          <NavLink />
+          <NavLink />
         </ul>
       </nav>
-      <section className="ProductList">
-        <h1>Products</h1>
-        <article>
-          <h2 className="ProductName">Product</h2>
-          <p className="Description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, quas tenetur. Assumenda laboriosam, minima dignissimos quae sequi alias quidem voluptatem nemo. Aliquam quibusdam provident est aperiam debitis cum repudiandae obcaecati!
-          </p>
-        </article>
-        <article>
-          <h2 className="ProductName">Product</h2>
-          <p className="Description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, quas tenetur. Assumenda laboriosam, minima dignissimos quae sequi alias quidem voluptatem nemo. Aliquam quibusdam provident est aperiam debitis cum repudiandae obcaecati!
-          </p>
-        </article>
-        <article>
-          <h2 className="ProductName">Product</h2>
-          <p className="Description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, quas tenetur. Assumenda laboriosam, minima dignissimos quae sequi alias quidem voluptatem nemo. Aliquam quibusdam provident est aperiam debitis cum repudiandae obcaecati!
-          </p>
-        </article>
-        <article>
-          <h2 className="ProductName">Product</h2>
-          <p className="Description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, quas tenetur. Assumenda laboriosam, minima dignissimos quae sequi alias quidem voluptatem nemo. Aliquam quibusdam provident est aperiam debitis cum repudiandae obcaecati!
-          </p>
-        </article>
-        <article>
-          <h2 className="ProductName">Product</h2>
-          <p className="Description">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, quas tenetur. Assumenda laboriosam, minima dignissimos quae sequi alias quidem voluptatem nemo. Aliquam quibusdam provident est aperiam debitis cum repudiandae obcaecati!
-          </p>
-        </article>
+    </header>
+  );
+}
+
+function ProdustList() {
+  return (
+    <section className="ProductList">
+      <h1>Products</h1>
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </section>
+  );
+}
+
+function Product() {
+  return (
+    <article className="Product">
+      <h2 className="ProductName">Product</h2>
+      <p className="Description">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, quas tenetur. Assumenda laboriosam, minima dignissimos quae sequi alias quidem voluptatem nemo. Aliquam quibusdam provident est aperiam debitis cum repudiandae obcaecati!
+      </p>
+    </article>
+  );
+}
+
+function NavLink() {
+  return (
+    <li>
+      <a href="/">Home</a>
+    </li>
+  );
+}
+
+function Basket() {
+  return (
+    <aside className="Basket">
+      <section className="MyBasket">
+        <h1>Basket</h1>
+        <ul>
+          <li>Total items</li>
+          <li>Price</li>
+        </ul>
       </section>
-      <aside>
-        <section className="MyBasket">
-          <ul>
-            <li>Total items</li>
-            <li>Price</li>
-          </ul>
-        </section>
-        <form className="CheckoutForm"></form>
-      </aside>
-    </main>
+      <form className="CheckoutForm"></form>
+    </aside>
   );
 }
 
