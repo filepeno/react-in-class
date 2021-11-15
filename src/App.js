bc182321205ac8e34365d0c314d578bc32c15110
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-15 15:20:06 +0100

    pass down props to product and basket

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 
 function App() {
+  const product = {
+    id: 1163,
+    productdisplayname: "Round Neck Jersey",
+    price: 895,
+    soldOut: 0,
+  };
+  const basket = [];
   return (
     <main>
       <Header />
-      <ProdustList />
-      <Basket />
+      <ProdustList {...product} />
+      <Basket {...[basket]} />
     </main>
   );
 }
@@ -25,28 +32,27 @@ function Header() {
   );
 }
 
-function ProdustList() {
+function ProdustList(props) {
   return (
     <section className="ProductList">
       <h1>Products</h1>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      <Product {...props} />
+      <Product {...props} />
+      <Product {...props} />
+      <Product {...props} />
+      <Product {...props} />
+      <Product {...props} />
     </section>
   );
 }
 
-function Product() {
+function Product(props) {
   return (
     <article className="Product">
-      <h2 className="ProductName">Product</h2>
+      <h2 className="ProductName">{props.productdisplayname}</h2>
+      <h3 className="Price">
+        <span>{props.price}</span> dkk
+      </h3>
       <p className="Description">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, quas tenetur. Assumenda laboriosam, minima dignissimos quae sequi alias quidem voluptatem nemo. Aliquam quibusdam provident est aperiam debitis cum repudiandae obcaecati!
       </p>
@@ -62,13 +68,16 @@ function NavLink() {
   );
 }
 
-function Basket() {
+function Basket(props) {
+  console.log(props);
   return (
     <aside className="Basket">
       <section className="MyBasket">
         <h1>Basket</h1>
         <ul>
-          <li>Total items</li>
+          <li>
+            You have <span>{props[0].length}</span> items in the basket
+          </li>
           <li>Price</li>
         </ul>
       </section>
