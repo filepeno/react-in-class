134f25b57e75835f7774721b7a2c024d2e65d69a
Filip Soudakov <fili4252@stud.kea.dk>
2021-11-19 10:36:05 +0100

    added a loader

--- src/components/Productlist.jsx
+++ src/components/Productlist.jsx
@@ -1,11 +1,15 @@
 import Product from "./Product";
-
+import PacmanLoader from "react-spinners/PacmanLoader";
+import { useState } from "react";
 export default function ProdustList(props) {
+  let [loading, setLoading] = useState(true);
+
   console.log(props);
   const productlist = props.products.map((product) => <Product {...product} key={product.id} {...props} />);
   return (
     <section className="ProductList">
       <h1>Products</h1>
+      <PacmanLoader color="blue" loading={loading} size={25} />
       {productlist}
     </section>
   );
